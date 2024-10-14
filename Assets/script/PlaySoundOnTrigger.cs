432a07f952bd217f22d88671e177ab7856cc9a94
MasaMalkawi <masamalkawi@gmail.com>
2024-10-14 20:30:57 +0300

    14/10/2024

---
+++ Assets/script/PlaySoundOnTrigger.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class PlaySoundOnTrigger : MonoBehaviour
+{
+    public AudioClip guideSound;  // الصوت الذي تريد تشغيله
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        // إعداد AudioSource لتشغيل الصوت
+        audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.clip = guideSound;
+    }
+
+    // يتفعل عند دخول اللاعب في المنطقة الوهمية
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Controller entered the zone!");  // سيظهر هذا في الـConsole عند دخول الـController
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+    }
+}
+
