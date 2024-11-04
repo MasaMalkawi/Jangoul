a42d7e27544bf442d03e302672b77a2168f239e6
MasaMalkawi <masamalkawi@gmail.com>
2024-11-04 13:47:51 +0300

    11/4

---
+++ Assets/script/PlayerControllerSetup.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class PlayerControllerSetup : MonoBehaviour
+{
+    void Start()
+    {
+        // تعيين الـTag إلى "Player"
+        gameObject.tag = "Player";
+
+        // الحصول على الـCollider
+        Collider col = GetComponent<Collider>();
+
+        // التأكد من أن الـCollider مُعَدّ كـ Trigger
+        if (!col.isTrigger)
+        {
+            col.isTrigger = true;
+            Debug.Log($"{gameObject.name}: Collider was set to Trigger.");
+        }
+    }
+}
+
---
+++ Assets/script/TriggerZone.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class TriggerZone : MonoBehaviour
+{
+    public AudioSource guideAudio;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && guideAudio != null)
+        {
+            if (!guideAudio.isPlaying)
+            {
+                guideAudio.Play();
+                Debug.Log("Player entered the trigger zone and audio is playing.");
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player") && guideAudio != null)
+        {
+            guideAudio.Stop();
+            Debug.Log("Player exited the trigger zone and audio stopped.");
+        }
+    }
+}
+
+
